7b6851c927a6300fdefb3b8a35932df427de31d7
Marcus Bastos <mvcbastos@gmail.com>
2020-10-01 12:00:47 -0300

    Piece move count

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -88,9 +88,10 @@ public class ChessMatch {
 	
 	private Piece makeMove(Position source, Position target) {
 		
-		Piece p = this.board.removePiece(source);
+		ChessPiece p = (ChessPiece) this.board.removePiece(source);
+		p.increaseMoveCount();
 		Piece capturedPiece = this.board.removePiece(target);
-		this.board.placePiece(p, target);
+		this.board.placePiece(p, target); // Aqui é feito um upcasting naturalmente
 		
 		if(capturedPiece != null) {
 			this.piecesOnTheBoard.remove(capturedPiece);
@@ -101,7 +102,8 @@ public class ChessMatch {
 	}
 	
 	private void undoMove(Position source, Position target, Piece capturedPiece) {
-		Piece p = this.board.removePiece(target);
+		ChessPiece p = (ChessPiece) this.board.removePiece(target);
+		p.decreaseMoveCount();
 		this.board.placePiece(p, source);
 		
 		if(capturedPiece != null) {
