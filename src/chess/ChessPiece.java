b62967077c0f5d1c9b93a972b73026d4667faf39
Marcus Bastos <mvcbastos@gmail.com>
2020-10-01 11:30:20 -0300

    Check logic

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -17,6 +17,11 @@ public abstract class ChessPiece extends Piece {
 		return color;
 	}
 	
+	public ChessPosition getChessPosition() {
+		// O método é estático, então eu posso executar uma chamada da forma abaixo
+		return ChessPosition.fromPosition(this.position);
+	}
+	
 	protected boolean isThereOpponentPiece(Position position) {
 		ChessPiece p = (ChessPiece) getBoard().piece(position);
 		return p != null && p.getColor() != this.color;
